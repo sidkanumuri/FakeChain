2fcf5609ad29a1148834424d1cf38ba053d18128
Sidharth <sidharth.kanumuri@varsun.com>
2018-07-23 00:07:45 -0700

    app

--- app/index.js
+++ app/index.js
@@ -9,11 +9,11 @@ const bc = new Blockchain();
 
 app.use(bodyParser.json());
 
-app.get('/blocks', (req, res) =>{
+app.get('/blocks', (req, res) => {
     res.json(bc.chain);
 });
 
-app.post('/mine', (req, res) =>{
+app.post('/mine', (req, res) => {
     const block = bc.addBlock(req.body.data);
     console.log(`New block added: ${block.toString()}`);
 
