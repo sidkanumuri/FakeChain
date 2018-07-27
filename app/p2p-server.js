55556e85bb48127f12d067fcc14547f2334fe2c8
Sidharth <sidharth.kanumuri@varsun.com>
2018-07-26 17:12:10 -0700

    initiated blockchain communication

--- app/p2p-server.js
+++ app/p2p-server.js
@@ -32,6 +32,17 @@ class P2pServer {
     connectSocket(socket){
         this.sockets.push(socket);
         console.log('socket connected');
+
+        this.messageHandler(socket);
+
+        socket.send(JSON.stringify(this.blockchain.chain));
+    }
+
+    messageHandler(socket){
+        socket.on('message', message => {
+            const data = JSON.parse(message);
+            console.log('data', data); 
+        });
     }
 }
 
