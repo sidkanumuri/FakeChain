cc2eb7e1af0317f1980bb041acaadc6118c4c5f3
Sidharth <sidharth.kanumuri@varsun.com>
2018-08-29 15:39:02 -0700

    reward transactions

--- wallet/transaction.js
+++ wallet/transaction.js
@@ -1,5 +1,5 @@
 const ChainUtil = require('../chain-util');
-
+const { MINING_REWARD } = require('../config');
 class Transaction {
   constructor() {
     this.id = ChainUtil.id();
@@ -21,6 +21,12 @@ class Transaction {
     
     return this;
   }
+  static transactionWithoutputs(senderWallet, outputs){
+    const transaction = new this();
+    transaction.outputs.push(...outputs);
+    Transaction.signTransaction(transaction, senderWallet);
+    return transaction;
+    }
 
   static newTransaction(senderWallet, recipient, amount) {
     if (amount > senderWallet.balance) {
@@ -30,12 +36,16 @@ class Transaction {
 
     const transaction = new this();
 
-    transaction.outputs.push(...[
+    return Transaction.transactionWithoutputs(senderWallet, [
       { amount: senderWallet.balance - amount, address: senderWallet.publicKey},
       { amount, address: recipient }
     ]);
-    Transaction.signTransaction(transaction, senderWallet);
-    return transaction;
+  }
+
+  static rewardTransaction(minerWallet, blockchainWallet){
+     return Transaction.transactionWithoutputs(blockchainWallet, [{
+       amount: MINING_REWARD, address: minerWallet.publicKey
+     }]);
   }
 
   static signTransaction(transaction, senderWallet) {
