e1d862292e523a52bda45c242a9865acd3779a14
Sidharth <sidharth.kanumuri@varsun.com>
2018-08-29 11:07:04 -0700

    added transaction pool to p2p server

--- app/p2p-server.js
+++ app/p2p-server.js
@@ -2,12 +2,16 @@ const Websocket = require('ws');
 
 const P2P_PORT = process.env.P2P_PORT || 5001;
 const peers = process.env.PEERS ? process.env.PEERS.split(',') : [];
-
+const MESSAGE_TYPES = {
+    chain: 'CHAIN',
+    transaction: 'TRANSACTION'
+};
 // $ HTTP_Port=3002 P2P_PORT=5003 PEERS= ws://localhost:5001,localhost:5002 npm run dev
 
 class P2pServer {
     constructor(blockchain){
         this.blockchain = blockchain;
+        this.transactionPool = this.transactionPool;
         this.sockets = [];
     }
 
@@ -48,11 +52,25 @@ class P2pServer {
     }
 
     sendChain(socket){
-        socket.send(JSON.stringify(this.blockchain.chain));
+        socket.send(JSON.stringify({
+        type: MESSAGE_TYPES.chain,
+        chain: this.blockchain.chain
+        }));
+    }
+
+    sendTransaction(socket, transaction){
+        socket.send(JSON.stringify({        
+        type: MESSAGE_TYPES.transaction,
+        transaction
+    }));
     }
 
     syncChains(){
             this.sockets.forEach(socket => this.sendChain(socket));
+    }
+
+    broadcastTransaction(transaction){
+        this.sockets.forEach(socket => this.sendTransaction(socket));
     }
 }
 
