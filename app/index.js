295ec1d81992056ed2d9b4bc897b693b3ed7acf5
Sidharth <sidharth.kanumuri@varsun.com>
2018-08-29 10:43:37 -0700

    post transactions completed

--- app/index.js
+++ app/index.js
@@ -2,15 +2,18 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const Blockchain = require('../blockchain');
 const P2pServer = require('./p2p-server');
+const Wallet = require('../wallet');
+const TransactionPool =require('../wallet/transaction-pool');
 
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
 
 const app = express();
 const bc = new Blockchain();
+const wallet = new Wallet();
+const tp = new TransactionPool();
 const p2pServer = new P2pServer(bc);
 
 app.use(bodyParser.json());
-
 app.get('/blocks', (req, res) => {
     res.json(bc.chain);
 });
@@ -23,6 +26,16 @@ app.post('/mine', (req, res) => {
 
     res.redirect('/blocks');
 });
+
+app.get('/transactions', (req, res) => {
+    res.json(tp.transactions);
+});
+
+app.post('/transact', (req, res) => {
+    const { recipient, amount } = req.body;
+    const transaction = wallet.createTransaction(recipient, amount, tp);
+    res.redirect('/transactions');
+  });
 
 app.listen(HTTP_PORT, () => console.log(`Listening on port ${HTTP_PORT}`));
 p2pServer.listen();
